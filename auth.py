67c72ba320734d3e46eae9fcbacc43ce7231dde4
Baribeop Barry Leele <68976451+Baribeop@users.noreply.github.com>
2023-09-26 10:09:04 +0100

    Update auth.py
    add file

--- auth.py
+++ auth.py
@@ -1,59 +1,80 @@
-# Import necessary libraries
 from flask import Flask, request, jsonify
-import jwt
+from flask_bcrypt import Bcrypt
 import datetime
-import secrets
+import jwt
+# import secrets
 
-# Initialize Flask app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = ''
+bcrypt = Bcrypt(app)
 
-# Secret key for JWT encoding/decoding (keep this secret)
-SECRET_KEY = 'b0b208372dc5a496fb631b73f1522974'
+# Store user data securely (hashed passwords)
+users = {
+    'john': bcrypt.generate_password_hash('gun').decode('utf-8'),
+    'grace': bcrypt.generate_password_hash('pig').decode('utf-8')
+}
 
+def create_access_token(username):
+    payload = {
+        'user': username,
+        'exp': datetime.datetime.now() + datetime.timedelta(hours=1)
+    }
+    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+    # print(token)
+    return token
 
-# secrets = secrets.token_hex(16)
-# print(secrets)
+@app.route('/')
+def home():
+    return 'Hello'
 
-# Temporary storage for user data (should be a database in a real system)
-users = {'mary': 'obi', 'joe': 'apple'}
-
-# Endpoint to obtain a token (authentication)
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    username = data['username']
-    password = data['password']
+    username = data.get('username')
+    password = data.get('password')
 
-    # Check if the provided username and password match the stored data
-    if username in users and users[username] == password:
-        # Generate a JWT token with user information
-        token = jwt.encode({'username': username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)},
-                           SECRET_KEY, algorithm='HS256')
-        return jsonify({'token': token.decode('UTF-8')})
+    if username in users and bcrypt.check_password_hash(users[username], password):
+        access_token = create_access_token(username)
+        return jsonify(message="Login succeeded!", access_token=access_token), 200
+    else:
+        return jsonify(message='Authentication failed, invalid login credential'), 401
 
-    return jsonify({'message': 'Authentication failed'}), 401
-
-# Protected GET endpoint
-@app.route('/data', methods=['GET'])
-def get_data():
+@app.route('/user', methods=['GET'])
+def get_user():
+    token = request.headers.get('Authorization').split(" ")[1]
+    if not token:
+        return jsonify(message='Token is missing'), 401
     try:
-        # Get the token from the request header
-        token = request.headers.get('Authorization').split(" ")[1]
-        # Decode the token using the secret key
-        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=['HS256'])
-        username = decoded_token['username']
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithm='HS256')
+        print(payload)
+        user = payload['user']
+        return jsonify(message='Authorization is successful', user=user), 200
+    except jwt.ExpiredSignatureError:
+        return jsonify(message='Expired token'), 401
+    except jwt.DecodeError:
+        return jsonify(message='Invalid token'), 401
 
-        # For example, you can retrieve user-specific data
-        return jsonify({'message': f'Hello, {username}! This is your protected data.'})
+@app.route('/create', methods=['POST'])
+def create_data():
+    token = request.headers.get('Authorization')
+    if not token:
+        return jsonify(message='Token is missing'), 401
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        username = request.json.get('username')
+        password = request.json.get('password')
 
-    except Exception as e:
-        return jsonify({'message': 'Authentication failed'}), 401
+        if username in users:
+            return jsonify(message='Username already exists'), 400
 
-# Run the application
+        # Hash the password before storing it
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        users[username] = hashed_password
+        return jsonify(message='Data created successfully'), 201
+    except jwt.ExpiredSignatureError:
+        return jsonify(message='Token has expired'), 401
+    except jwt.DecodeError:
+        return jsonify(message='Invalid token'), 401
+
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-
+    app.run(debug=True, port=8000)
